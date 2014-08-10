a3d7b7dc28037ed0827c420da9e957fcbee6f681
Carlos Maniero <carlosmaniero@gmail.com>
2014-08-10 02:10:09 -0300

    On read

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,14 @@
-var liveBackground = new LiveBackground($('#live-background'));
+$(function(){
+    var liveBackground = new LiveBackground($('#live-background'));
 
-var elLogo = document.getElementById('logo');
-var logo = new Sentinel({ 
-    obj: elLogo, 
-    file: 'assets/img/logo.png',
-    ppf: 100,
-    size: 6,
-    completeCallback: function(){
-        $("#home h2").fadeIn(500)
-    }
+    var elLogo = document.getElementById('logo');
+    var logo = new Sentinel({ 
+        obj: elLogo, 
+        file: 'assets/img/logo.png',
+        ppf: 100,
+        size: 6,
+        completeCallback: function(){
+            $("#home h2").fadeIn(500)
+        }
+    });
 });
